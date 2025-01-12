17e053f998b9eb2a5533e2bd5e5dfcef74af59f0
Gazzito <telmo9@hotmail.com>
2025-01-12 02:41:35 +0000

    Accept Privacy Policy Popup developed

---
+++ app/src/main/java/com/example/arcanedex_app/PrivacyPolicyAgreement.kt
@@ -0,0 +1,20 @@
+package com.example.arcanedex_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PrivacyPolicyAgreement : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_privacy_policy_agreement)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
