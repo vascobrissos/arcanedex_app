b6241852d490818f7215b4606574f713f714ae48
Gazzito <telmo9@hotmail.com>
2025-01-13 00:03:34 +0000

    BottomBar stack reseted

--- app/src/main/java/com/example/arcanedex_app/Home.kt
+++ app/src/main/java/com/example/arcanedex_app/Home.kt
@@ -1,10 +1,8 @@
 package com.example.arcanedex_app
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,23 +11,37 @@ class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_home)
 
-        try {
-            // Vincule o NavController
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragments) as NavHostFragment
-            val navController = navHostFragment.navController
-            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        // Configure o NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragments) as NavHostFragment
+        val navController = navHostFragment.navController
 
+        // Configure o BottomNavigationView
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
 
-            bottomNavigationView.setupWithNavController(navController)
-
-            Toast.makeText(this, "NavController configurado com sucesso", Toast.LENGTH_SHORT).show()
-            Log.d("HomeActivity", "NavController configurado com sucesso")
-        } catch (e: Exception) {
-            Toast.makeText(this, "Erro ao configurar NavController", Toast.LENGTH_SHORT).show()
-            Log.e("HomeActivity", "Erro ao configurar NavController", e)
+        // Adicione listener para redefinir a pilha de navegação
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navController.popBackStack(R.id.homeFragment, false)
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.favouritesFragment -> {
+                    navController.popBackStack(R.id.favouritesFragment, false)
+                    navController.navigate(R.id.favouritesFragment)
+                    true
+                }
+                R.id.profileFragment -> {
+                    navController.popBackStack(R.id.profileFragment, false)
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
