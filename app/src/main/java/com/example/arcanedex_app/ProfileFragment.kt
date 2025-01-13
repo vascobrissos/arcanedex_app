8e9794160326987f071ff026aa971c1d6acd02ca
Gazzito <telmo9@hotmail.com>
2025-01-13 04:49:46 +0000

    Settings Page UI Done

--- app/src/main/java/com/example/arcanedex_app/ProfileFragment.kt
+++ app/src/main/java/com/example/arcanedex_app/ProfileFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.arcanedex_app.data.utils.SharedPreferencesHelper
 
 class ProfileFragment : Fragment() {
@@ -15,7 +17,16 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Find the settingsButton by ID and set an OnClickListener
+        val settingsButton = view.findViewById<ImageButton>(R.id.settingsButton)
+        settingsButton.setOnClickListener {
+            // Navigate to SettingsFragment
+            findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
+        }
+
+        return view
     }
 
     fun logout(view: View?) {
